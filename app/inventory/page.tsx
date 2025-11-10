262dea564ccbbc5bc6b86e0b97035500c6fb2323
doresimi <filipus.535240211@stu.untar.ac.id>
2025-11-10 12:47:17 +0700

    added inventory

---
+++ app/inventory/page.tsx
@@ -0,0 +1,16 @@
+
+'use client'
+import React, { useState } from 'react';
+
+interface InventoryItem {
+  id: number;
+  name: string;
+  stock: number;
+  price: number;
+}
+
+
+
+export default function Inventory() {
+ 
+}
