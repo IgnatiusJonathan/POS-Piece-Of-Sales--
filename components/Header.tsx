eef8e2d3ad7dbe82312406c28518f362ef5bf932
IgnatiusJonathan <ignatiusjonathantjandra@gmail.com>
2025-12-03 11:22:23 +0700

    Fixed header

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 const Header = () => {
   const [name, setName] = useState<string | null>(null);  // Define role as string or null
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   useEffect(() => {
     const savedName = localStorage.getItem('nama');
@@ -16,20 +17,24 @@ const Header = () => {
     return 'Cashier ▼';
   };
 
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-left">
         <h1><span className="brand">TARUMART</span> <span className="tagline">Kasir</span></h1>
       </div>
       <div className="header-right">
-        <div className="employee-login" id="profileMenu">
+        <div className="employee-login" id="profileMenu" onClick={toggleDropdown}>
           <span className="cashier-name">{getNameText()}</span>
           <div className="profile-icon">👤</div>
         </div>
 
         {/* Dropdown logout */}
-        <div className="dropdown" id="dropdownMenu">
-          <Link href="../login.html">
+        <div className={`dropdown ${isDropdownOpen ? 'show' : ''}`} id="dropdownMenu">
+          <Link href="/login">
             <img src="../img/logout.png" alt="Logout" className="dropdown-icon" />
             Logout
           </Link>
