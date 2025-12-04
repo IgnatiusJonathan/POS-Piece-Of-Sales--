6bcd5198ba8a2c2ce71f7951430c5931a54db3b3
LaCacoythe <160859548+LaCacoythe@users.noreply.github.com>
2025-12-04 23:35:32 +0700

    Update prisma.ts
    Edit link database agar lebih jelas

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,10 @@
-import {PrismaClient} from 
-'@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: 'file:./dev.db',
+        },
+    },
+});
 export default prisma;
-                    
