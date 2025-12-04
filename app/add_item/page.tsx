f096bfcf873e8782bebd332ad6eb9c354475e017
LaCacoythe <160859548+LaCacoythe@users.noreply.github.com>
2025-12-04 23:16:41 +0700

    Update page.tsx
    Edit agar menggunakan komponen Navbar, bukan navbar

--- app/add_item/page.tsx
+++ app/add_item/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import { useNavbar } from "@/context/NavbarContext";
 import { useRouter } from "next/navigation";
 
