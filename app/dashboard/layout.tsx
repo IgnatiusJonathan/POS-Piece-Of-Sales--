1829000be024213417488549cfa7c0f74b2b46cd
LaCacoythe <160859548+LaCacoythe@users.noreply.github.com>
2025-12-04 23:18:55 +0700

    Update layout.tsx
    Ubah dari navbar ke Navbar agar komponen Navbar bisa digunakan

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import Header from '@/components/Header'
-import Navbar from '@/components/navbar'
+import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
 export default function DashboardLayout({
