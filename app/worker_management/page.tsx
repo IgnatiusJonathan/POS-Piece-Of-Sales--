eca93058d62aa828cd9c6056482321c791666e58
LaCacoythe <160859548+LaCacoythe@users.noreply.github.com>
2025-12-04 23:30:30 +0700

    Update page.tsx
    Update agar menggunakan komponen Navbar

--- app/worker_management/page.tsx
+++ app/worker_management/page.tsx
@@ -3,7 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Navbar from "@/components/navbar";
+import Navbar from "@/components/Navbar";
 import { getWorkers, createWorker, deleteWorker, getHistory, getProducts, getLastUpdate } from './actions';
 
 interface Worker {
